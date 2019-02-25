61d018acec655060b6d1a137ee8dc3872729adf4
Ashutosh Shukla <asshukla@nvidia.com>
2019-02-24 18:42:08 -0800

    removing unwanted URLs

--- app/src/main/java/com/example/bookstorechain/UrlConstants.java
+++ app/src/main/java/com/example/bookstorechain/UrlConstants.java
@@ -2,11 +2,6 @@ package com.example.bookstorechain;
 
 public interface UrlConstants {
 
-    String BRANCHES_URL1 = "http://34.73.67.162:8080/branches";
     String BRANCHES_URL = "http://34.73.67.162:8080/branches";
     String BOOKS_URL = "http://34.73.67.162:8080/books";
-    String VIEW_GET_URL = "https://mgaitec6450finalproject.azurewebsites.net/report/"; // "http://10.0.2.2:8080/report/";
-    String DELETE_URL =   "https://mgaitec6450finalproject.azurewebsites.net/report/";  // "http://10.0.2.2:8080/report/";
-    String POST_URL = "https://mgaitec6450finalproject.azurewebsites.net/activities/testUser"; // "http://10.0.2.2:8080/activities/testUser";
-    String USER_NAME = "testUser";
-}
+ }
